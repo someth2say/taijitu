ad3cc21d2905546d0ee06033b6600abdbe2f21ac
Jordi Sola <someth2say@gmail.com>
2018-01-04 15:46:35 +0100

    Little bit more clean

--- cli/src/main/java/org/someth2say/taijitu/cli/TaijituCliRunner.java
+++ cli/src/main/java/org/someth2say/taijitu/cli/TaijituCliRunner.java
@@ -1,5 +1,6 @@
 package org.someth2say.taijitu.cli;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.someth2say.taijitu.cli.config.interfaces.IComparisonCfg;
@@ -50,41 +51,28 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
         return runComparison(comparisonCfg);
     }
 
-    private class SourceData<GENERATED_TYPE, MAPPED_TYPE> {
-        final ISourceCfg sourceCfg;
-        Source<GENERATED_TYPE> source;
-        Source<MAPPED_TYPE> mappedSource;
-        SourceMapper<GENERATED_TYPE, MAPPED_TYPE> mapper;
-
-        private SourceData(ISourceCfg sourceCfg) {
-            this.sourceCfg = sourceCfg;
-        }
-    }
-
-    public <GENERATED_TYPE, MAPPED_TYPE> Stream<Difference<?>> runComparison(IComparisonCfg iComparisonCfg) {
+    public <MAPPED_TYPE> Stream<Difference<?>> runComparison(IComparisonCfg iComparisonCfg) {
 
         List<ISourceCfg> sourceConfigs = iComparisonCfg.getSourceConfigs();
         if (sourceConfigs.size() < 2) {
-            throw new RuntimeException(
-                    "There should be at least 2 sources configured, but only " + sourceConfigs.size() + " found");
+            throw new RuntimeException("There should be at least 2 sources configured, but only " + sourceConfigs.size() + " found");
         }
         if (sourceConfigs.size() > 2) {
             logger.warn("More than 2 sources found ({}}). Only first two will be considered!", sourceConfigs.size());
         }
 
         // 0. Build and map sources
-        List<SourceData<GENERATED_TYPE, MAPPED_TYPE>> sourceDatas = buildSourceDatas(sourceConfigs);
+        Duo<Source<MAPPED_TYPE>> mappedSources = buildMappedSources(sourceConfigs);
 
         // 2. Calculate common fields (given all sources)
-        List<Source<MAPPED_TYPE>> mappedSources = sourceDatas.stream().map(sd -> sd.mappedSource).collect(Collectors.toList());
         final StreamEqualizer<MAPPED_TYPE> streamEquality = getStreamEqualizer(iComparisonCfg, mappedSources);
 
         logger.info("Comparison {} ready to run.", iComparisonCfg.getName());
-        return runStreamEquality(streamEquality, sourceDatas);
+        return runStreamEquality(streamEquality, mappedSources);
     }
 
     private <MAPPED_TYPE> StreamEqualizer<MAPPED_TYPE> getStreamEqualizer(IComparisonCfg iComparisonCfg,
-                                                                          List<Source<MAPPED_TYPE>> mappedSources) {
+                                                                          Duo<Source<MAPPED_TYPE>> mappedSources) {
         Map<String, FieldDescription<?>> commonFDs = getCommonFields(mappedSources);
         List<FieldDescription<?>> keyFDs = iComparisonCfg.getKeyFields().stream().map(commonFDs::get).collect(Collectors.toList());
         List<FieldDescription<?>> sortFDs = iComparisonCfg.getSortFields().stream().map(commonFDs::get).collect(Collectors.toList());
@@ -110,7 +98,7 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
             throw new RuntimeException("Unable to define comparison fields!");
         } else {
             if (sorter != null && hasher != null) {
-                throw new RuntimeException("Hybrid equality not supported yet. Please use only Keys or Sort fields");
+                throw new RuntimeException("Hybrid stream equality not supported yet. Please use only Keys or Sort fields");
             } else {
                 if (hasher != null) {
                     streamEquality = new HashingStreamEqualizer<>(equality, hasher);
@@ -128,13 +116,14 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
                                                                       List<FieldDescription<?>> keyFDs,
                                                                       List<FieldDescription<?>> sortFDs,
                                                                       List<String> compareFDS,
-                                                                      List<Source<MAPPED_TYPE>> mappedSources,
+                                                                      Duo<Source<MAPPED_TYPE>> mappedSources,
                                                                       List<IEqualityCfg> equalityConfigs) {
         CompositeEqualizer<MAPPED_TYPE> equality = null;
-        List<FieldDescription<?>> actualCompareFDs =
-                (!compareFDS.isEmpty()
-                        ? commonFDs.values().stream().filter(commonFD -> compareFDS.contains(commonFD.getName())) //TODO: Check that all compare fields actually are provided
-                        : commonFDs.values().stream().filter(commonFD -> !keyFDs.contains(commonFD) && !sortFDs.contains(commonFD))).collect(Collectors.toList());
+        Stream<FieldDescription<?>> commonFDsStream = commonFDs.values().stream();
+        List<FieldDescription<?>> actualCompareFDs = (!compareFDS.isEmpty()
+                        ? commonFDsStream.filter(commonFD -> compareFDS.contains(commonFD.getName()))
+                        : commonFDsStream.filter(commonFD -> !keyFDs.contains(commonFD) && !sortFDs.contains(commonFD)))
+                        .collect(Collectors.toList());
         if (!actualCompareFDs.isEmpty()) {
             CompositeEqualizer.Builder<MAPPED_TYPE> equalityBuilder = new CompositeEqualizer.Builder<>();
             actualCompareFDs.forEach(fd -> addEqualityComponent(equalityBuilder, fd, mappedSources, equalityConfigs));
@@ -143,9 +132,9 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
         return equality;
     }
 
-    private <GENERATED_TYPE, MAPPED_TYPE> CompositeComparator<MAPPED_TYPE> getComparator(List<FieldDescription<?>> sortFDs,
-                                                                                         List<Source<MAPPED_TYPE>> mappedSources,
-                                                                                         List<IEqualityCfg> equalityConfigs) {
+    private <MAPPED_TYPE> CompositeComparator<MAPPED_TYPE> getComparator(List<FieldDescription<?>> sortFDs,
+                                                                         Duo<Source<MAPPED_TYPE>> mappedSources,
+                                                                         List<IEqualityCfg> equalityConfigs) {
         CompositeComparator<MAPPED_TYPE> sorter = null;
         if (!sortFDs.isEmpty()) {
             CompositeComparator.Builder<MAPPED_TYPE> comparerBuilder = new CompositeComparator.Builder<>();
@@ -156,7 +145,7 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
     }
 
     private <MAPPED_TYPE> CompositeHasher<MAPPED_TYPE> getHasher(List<FieldDescription<?>> keyFDs,
-                                                                 List<Source<MAPPED_TYPE>> mappedSources,
+                                                                 Duo<Source<MAPPED_TYPE>> mappedSources,
                                                                  List<IEqualityCfg> equalityConfigs) {
         CompositeHasher<MAPPED_TYPE> hasher = null;
         if (!keyFDs.isEmpty()) {
@@ -169,9 +158,9 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
 
     private <MAPPED_TYPE, VALUE_TYPE> void addEqualityComponent(CompositeEqualizer.Builder<MAPPED_TYPE> equalityBuilder,
                                                                 FieldDescription<VALUE_TYPE> fd,
-                                                                List<Source<MAPPED_TYPE>> mappedSources,
+                                                                Duo<Source<MAPPED_TYPE>> mappedSources,
                                                                 List<IEqualityCfg> equalityConfigs) {
-        Source<MAPPED_TYPE> mappedSource = mappedSources.get(0);
+        Source<MAPPED_TYPE> mappedSource = mappedSources.getLeft(); // Here we are assuming the all same extractor can be used for all sources (a.k.a. non-hybrid equality)
         Function<MAPPED_TYPE, VALUE_TYPE> extractor = mappedSource.getExtractor(fd);
         Equalizer<VALUE_TYPE> vEqualizer = getEquality(fd, equalityConfigs);
         equalityBuilder.addComponent(extractor, vEqualizer);
@@ -179,9 +168,9 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
 
     private <MAPPED_TYPE, VALUE_TYPE> void addCategorizerComponent(CompositeHasher.Builder<MAPPED_TYPE> categorizerBuilder,
                                                                    FieldDescription<VALUE_TYPE> fd,
-                                                                   List<Source<MAPPED_TYPE>> mappedSources,
+                                                                   Duo<Source<MAPPED_TYPE>> mappedSources,
                                                                    List<IEqualityCfg> equalityConfigs) {
-        Source<MAPPED_TYPE> mappedSource = mappedSources.get(0); // Here we are assuming the all same extractor can be used for all sources (a.k.a. non-hybrid equality)
+        Source<MAPPED_TYPE> mappedSource = mappedSources.getLeft(); // Here we are assuming the all same extractor can be used for all sources (a.k.a. non-hybrid equality)
         Function<MAPPED_TYPE, VALUE_TYPE> extractor = mappedSource.getExtractor(fd);
         Hasher<VALUE_TYPE> vEquality = getCategorizerEquality(fd, equalityConfigs);
         categorizerBuilder.addComponent(extractor, vEquality);
@@ -189,23 +178,22 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
 
     private <MAPPED_TYPE, VALUE_TYPE> void addComparerComponent(CompositeComparator.Builder<MAPPED_TYPE> comparerBuilder,
                                                                 FieldDescription<VALUE_TYPE> fd,
-                                                                List<Source<MAPPED_TYPE>> mappedSources,
+                                                                Duo<Source<MAPPED_TYPE>> mappedSources,
                                                                 List<IEqualityCfg> equalityConfigs) {
-        Source<MAPPED_TYPE> mappedSource = mappedSources.get(0); // Here we are assuming the all same extractor can be used for all sources (a.k.a. non-hybrid equality)
+        Source<MAPPED_TYPE> mappedSource = mappedSources.getLeft(); // Here we are assuming the all same extractor can be used for all sources (a.k.a. non-hybrid equality)
         Function<MAPPED_TYPE, VALUE_TYPE> extractor = mappedSource.getExtractor(fd);
         Comparator<VALUE_TYPE> vEquality = getComparableEquality(fd, equalityConfigs);
         comparerBuilder.addComponent(extractor, vEquality);
     }
 
-    private <GENERATED_TYPE, MAPPED_TYPE> List<SourceData<GENERATED_TYPE, MAPPED_TYPE>> buildSourceDatas(List<ISourceCfg> sourceConfigs) {
-        return sourceConfigs.stream().limit(2)
-                .map(sourceConfig -> {
-                    SourceData<GENERATED_TYPE, MAPPED_TYPE> sourceData = new SourceData<>(sourceConfig);
-                    sourceData.source = buildSource(sourceConfig);
-                    sourceData.mapper = buildMapper(sourceConfig);
-                    sourceData.mappedSource = mapSource(sourceData.source, sourceData.mapper);
-                    return sourceData;
-                }).collect(Collectors.toList());
+    private <MAPPED_TYPE> Duo<Source<MAPPED_TYPE>> buildMappedSources(List<ISourceCfg> sourceConfigs) {
+        return new Duo<>(getMappedSource(sourceConfigs.get(0)), getMappedSource(sourceConfigs.get(1)));
+    }
+
+    private <GENERATED_TYPE, MAPPED_TYPE> Source<MAPPED_TYPE> getMappedSource(ISourceCfg sourceConfig) {
+        Source<GENERATED_TYPE> source = buildSource(sourceConfig);
+        SourceMapper<GENERATED_TYPE, MAPPED_TYPE> mapper = buildMapper(sourceConfig);
+        return mapSource(source, mapper);
     }
 
     private <GENERATED_TYPE, MAPPED_TYPE> Source<MAPPED_TYPE> mapSource(Source<GENERATED_TYPE> source, SourceMapper<GENERATED_TYPE, MAPPED_TYPE> mapper) {
@@ -241,11 +229,10 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
         return source;
     }
 
-    private <GENERATED_TYPE, MAPPED_TYPE> Stream<Difference<?>> runStreamEquality(StreamEqualizer<MAPPED_TYPE> streamEquality, List<SourceData<GENERATED_TYPE, MAPPED_TYPE>> sourceDatas) {
-        SourceData<?, MAPPED_TYPE> sourceData0 = sourceDatas.get(0);
-        SourceData<?, MAPPED_TYPE> sourceData1 = sourceDatas.get(1);
-        Source<MAPPED_TYPE> sourceSrc = sourceData0.mappedSource;
-        Source<MAPPED_TYPE> targetSrc = sourceData1.mappedSource;
+    private <MAPPED_TYPE> Stream<Difference<?>> runStreamEquality(StreamEqualizer<MAPPED_TYPE> streamEquality,
+                                                                  Duo<Source<MAPPED_TYPE>> mappedSources) {
+        Source<MAPPED_TYPE> sourceSrc = mappedSources.getLeft();
+        Source<MAPPED_TYPE> targetSrc = mappedSources.getRight();
         Stream<MAPPED_TYPE> sourceStream = sourceSrc.stream();
         Stream<MAPPED_TYPE> targetStream = targetSrc.stream();
         return streamEquality.underlyingDiffs(sourceStream, targetStream);
@@ -346,12 +333,12 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
         }
     }
 
-    private <MAPPED_TYPE> Map<String, FieldDescription<?>> getCommonFields(List<Source<MAPPED_TYPE>> sources) {
+    private <MAPPED_TYPE> Map<String, FieldDescription<?>> getCommonFields(Duo<Source<MAPPED_TYPE>> sources) {
         return getCanonicalFDs(sources).stream().collect(Collectors.toMap(FieldDescription::getName, Function.identity()));
     }
 
-    private <MAPPED_TYPE> List<FieldDescription<?>> getCanonicalFDs(List<Source<MAPPED_TYPE>> sources) {
-        List<FieldDescription<?>> canonicalFDs = sources.get(0).getProvidedFields();
+    private <MAPPED_TYPE> List<FieldDescription<?>> getCanonicalFDs(Duo<Source<MAPPED_TYPE>> sources) {
+        List<FieldDescription<?>> canonicalFDs = sources.getLeft().getProvidedFields();
         // Retain only canonicalFields that are also provided by other streams.
         for (Source<MAPPED_TYPE> source : sources.stream().skip(1).collect(Collectors.toList())) {
             List<FieldDescription<?>> sourceFDs = source.getProvidedFields();
@@ -360,4 +347,33 @@ class TaijituCliRunner implements Callable<Stream<Difference<?>>> {
         return canonicalFDs;
     }
 
+}
+
+class Duo<T> extends Pair<T,T> {
+    private final T left;
+    private final T right;
+
+    Duo(final T left, final T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public T getLeft() {
+        return left;
+    }
+
+    @Override
+    public T getRight() {
+        return right;
+    }
+
+    @Override
+    public T setValue(T value) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Stream<T> stream() {
+        return Stream.of(left, right);
+    }
 }
