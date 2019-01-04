6de759ea036383873751dfad02cf04254fb88e0a
Jordi Sola <someth2say@gmail.com>
2019-01-04 16:05:19 +0000

    Fixed equalizer default areeEquals. Added some docs

--- taijitu/src/main/java/org/someth2say/taijitu/compare/equality/aspects/external/Equalizer.java
+++ taijitu/src/main/java/org/someth2say/taijitu/compare/equality/aspects/external/Equalizer.java
@@ -8,8 +8,17 @@ import java.util.stream.Stream;
 
 public interface Equalizer<EQUALIZED> {
 
+    /**
+     * Default implementation for checking equality delegates onto underlying differences.
+     * In case no differences are found, objects are considered equal. Else, are considered non-equal.,
+     * 
+     * Note that this implementation creates and consume a `Stream` of underlying differences. 
+     * Despite streams aer targeted to be lazy, efficiency depends on `underlyingDiffs` actual implementation and return type.
+     * 
+     * Equalizers for simple types (i.e. native types or value types) should overwrite this method and provide an straightforward implementation.
+     */
     default boolean areEquals(EQUALIZED equalized1, EQUALIZED equalized2) {
-        return underlyingDiffs(equalized1, equalized2).count() == 0;
+        return underlyingDiffs(equalized1, equalized2) == null;
     }
 
     /**
@@ -18,7 +27,10 @@ public interface Equalizer<EQUALIZED> {
      * to other equalizers, while stream equalizers apply the same equality to each element of the stream.
      * <p>
      * In case there is no difference between elements, null shall be returned.
-     *
+     * <p>
+     * If compared instances should be considered different, but no underlying differences (i.e. native types, Boolean or any other
+     * type that can not be decomposed), and empty stream should be returned.
+     * 
      * @param equalized1
      * @param equalized2
      * @return A stream of differences that explain the current differenceOrNull
